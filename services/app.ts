2cd1f26756120ec9d4b26635196d9040cd51422b
lightsaid <lightsaid@163.com>
2021-09-23 18:01:33 +0800

    modify: 修改为 typescript 例子

---
+++ services/app.ts
@@ -0,0 +1,61 @@
+~(function (window) {
+  function App() {
+    this.run = function () {
+      try {
+        const args = [].slice.call(arguments)
+        const fn = args.shift()
+        fn.apply(this, args)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+  }
+  function Foo() {
+    this.foo = "===foo==="
+  }
+  Foo.prototype.print = function () {
+    console.log(this.foo)
+  }
+  App.prototype = { Message: new Window.prototype.Message(), Foo: new Foo() }
+  Object.defineProperty(App.prototype, "constructor", {
+    enumerable: false,
+    value: App,
+    writable: true,
+  })
+
+  var app = new App()
+  app.Foo.print()
+  function done() {
+    console.log("~~done~~")
+  }
+  let instance = app.Message.success({
+    duration: 2000,
+    content: "成功~",
+    onClose: done,
+  })
+  // app.Message.close(instance, done)
+
+  // console.log(typeof instance)
+
+  const job = {
+    name: "zhansan",
+    work: function (slogan) {
+      //    try{
+      //     console.log(this.name + " ing...")
+      //     throw new Error("出毛病了。。。")
+      //    }catch(err){
+      //        console.log(err)
+      //    }
+      console.log(this.name + slogan)
+      throw new Error("出毛病了。。。")
+    },
+  }
+
+  //    job.work(' ing...')
+  app.run.call(job, job.work, " working...")
+  app.run.apply(job, [job.work, " ing..."])
+
+  console.log("=====如果不 try catch 后续不会执行=====aaabbb")
+
+  // app.Message.error();
+})(window)
--- utils/message.js
+++ utils/message.js
@@ -1,74 +1,67 @@
-~function(window){
-    function Message(){
-        let typesClass = {success:"icon-success-filling",error:"",warning:"",info:"",loading:""};
-        let messageWrapper = null
-        this.duration = 3000
-        this.setMessageWrapper = function(){
+~function (Window) {
+    function Message() {
+        var typesClass = { success: "icon-success-filling", error: "", warning: "", info: "", loading: "" };
+        var messageWrapper = null;
+        this.duration = 3000;
+        this.setMessageWrapper = function () {
             messageWrapper = document.getElementById("message-wrapper");
-            if(!messageWrapper){
+            if (!messageWrapper) {
                 var div = document.createElement("div");
                 div.id = "message-wrapper";
                 document.body.append(div);
-                messageWrapper = div
+                messageWrapper = div;
             }
-        }
-
+        };
         // 创建容器
-        this.setMessageWrapper()
-
-        this.create = function(config, iconName){
-            var div = document.createElement("div")
-            div.isDel = false
-            var icon = document.createElement("span")
-            var content = document.createElement("span")
-            icon.classList.add("iconfont")
-            icon.classList.add(iconName)
-            content.textContent = config.content
-            div.append(icon)
-            div.append(content)
-            return div
-        }
-
-        this.close = function(dom, configOrFunc){
-            if(dom && typeof configOrFunc === 'function'){
-                !dom.isDel && messageWrapper.removeChild(dom)
-                dom.isDel = !dom.isDel
-                configOrFunc()
-                return
+        this.setMessageWrapper();
+        this.create = function (config, iconName) {
+            var div = document.createElement("div");
+            div.isDel = false;
+            var icon = document.createElement("span");
+            var content = document.createElement("span");
+            icon.classList.add("iconfont");
+            icon.classList.add(iconName);
+            content.textContent = config.content;
+            div.append(icon);
+            div.append(content);
+            return div;
+        };
+        this.close = function (dom, configOrFunc) {
+            if (dom && typeof configOrFunc === 'function') {
+                !dom.isDel && messageWrapper.removeChild(dom);
+                dom.isDel = !dom.isDel;
+                configOrFunc();
+                return;
             }
-            let duration = configOrFunc.duration || this.duration
-            const timer = setTimeout(function(){
-                !dom.isDel && messageWrapper.removeChild(dom)
-                dom.isDel = !dom.isDel
-                configOrFunc.onClose && typeof configOrFunc.onClose === 'function' && configOrFunc.onClose()
-                clearTimeout(timer)
-            }, duration)
-        }
-
+            var duration = configOrFunc.duration || this.duration;
+            var timer = setTimeout(function () {
+                !dom.isDel && messageWrapper.removeChild(dom);
+                dom.isDel = !dom.isDel;
+                configOrFunc.onClose && typeof configOrFunc.onClose === 'function' && configOrFunc.onClose();
+                clearTimeout(timer);
+            }, duration);
+        };
         // config = { duration: 5000, content: "成功~", onClose: function }
-        this.success = function(config){
-            let div = this.create(config, typesClass.success)
-            messageWrapper.append(div)
-            this.close(div, config)
-            return div
-        }
+        this.success = function (config) {
+            var div = this.create(config, typesClass.success);
+            messageWrapper.append(div);
+            this.close(div, config);
+            return div;
+        };
         // this.error = function(){
         //     let div = this.create()
         //     messageWrapper.append(div)
         //     return div
         // }
-
-        const yoshi = { skulk: true }; 
-        const hattori = { sneak: true };
-        const kuma = { creep: true };  
+        var yoshi = { skulk: true };
+        var hattori = { sneak: true };
+        var kuma = { creep: true };
         Object.setPrototypeOf(yoshi, hattori);
         Object.setPrototypeOf(hattori, kuma);
-
-        console.log(yoshi)
-        console.log('skulk' in yoshi)
-        console.log('sneak' in yoshi)
-        console.log('creep' in yoshi)
+        console.log(yoshi);
+        console.log('skulk' in yoshi);
+        console.log('sneak' in yoshi);
+        console.log('creep' in yoshi);
     }
-    window.Message = Message
-}(window)
-
+    Window.prototype.Message = Message;
+}(Window);
